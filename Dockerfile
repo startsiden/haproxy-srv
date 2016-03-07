2a0519efe70166d142d23bdb9a0e442868f63a55
Nicholas Klem <nicholas@redpill-linpro.com>
2016-03-07 15:18:45 +0100

    haproxy med dns lookups

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM debian:jessie-backports
 # FROM ubuntu-debootstrap:14.04
 RUN apt-get update -q
 RUN DEBIAN_FRONTEND=noninteractive apt-get install -qy apt-utils
-RUN DEBIAN_FRONTEND=noninteractive apt-get install -qy curl netcat tcpdump
+RUN DEBIAN_FRONTEND=noninteractive apt-get install -qy curl netcat tcpdump dnsutils
 RUN DEBIAN_FRONTEND=noninteractive apt-get -t jessie-backports install -qy haproxy
 # RUN curl -sL https://deb.nodesource.com/setup_4.x | bash -
 # RUN DEBIAN_FRONTEND=noninteractive apt-get install -qy nodejs
--- k8s/build-and-push.sh
+++ k8s/build-and-push.sh
@@ -7,3 +7,12 @@ gcloud docker -a
 docker build -t eu.gcr.io/divine-arcade-95810/haproxy .
 docker push eu.gcr.io/divine-arcade-95810/haproxy
 
+# tips and tricks
+# 1233  07/03/16 14:42:14 kubectl get pods  --all-namespaces
+# 1235  07/03/16 14:48:53 kubectl get rc,services --all-namespaces
+# 1236  07/03/16 14:49:01 kubectl get rc,services --namespace=kube-system
+# 1237  07/03/16 14:55:21 gcloud container clusters get-credentials qa -z europe-west1-d
+
+
+
+for i in development qa production; do gcloud container clusters get-credentials $i -z europe-west1-d && kubectl replace -f rc.yaml; done
